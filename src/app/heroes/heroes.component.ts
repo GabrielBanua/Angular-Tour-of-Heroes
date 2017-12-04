0e439911749c23e4c7ece5f085ac8748b85ef6fd
Gabriel Francis M. Banua <banua.gabrielle10@gmail.com>
2017-12-04 12:47:58 +0800

    modified heroes.component.html and heroes.component.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,16 +6,15 @@ import { Hero } from '../hero';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {	
-	hero: Hero = {
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  	};
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
-  hero = 'Windstorm';
 
 }
